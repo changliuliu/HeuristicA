06e9d1bd8b6f9fe91eb08d36db59dbcad7cb2a2f
yizhou.wang <yizhou.wang@le.com>
2017-01-03 15:38:14 -0800

    minor change
    * add theta list
    * load csv map format

--- test_BLHeuristics.m
+++ test_BLHeuristics.m
@@ -1,13 +1,25 @@
 %% Setup Paramters
-load('map_for_test.mat');
+map = load('grid_worlds/maze.csv');
 Rmin = 10;
-theta_list = [pi/2];
+theta_list = [0.0, 26.565, 45.0, 63.435, 90.0, 116.565, 135.0, 153.435, 180.0, 206.565, 225.0, 243.435, 270.0, 296.565, 315.0, 333.435] / 180 * pi;
 
 %% Compute the BLHeuristics
 [Hmap,lines,corners] = BLHeuristics(map,Rmin,theta_list);
 
 %% Write to csv file
-filename = 'Heuristic.csv';
+% e.g. 40 * 80 grid cell with 16 angles 
+% theta's = [0.0, 26.565, 45.0, 63.435, 90.0, 116.565, 135.0, 153.435, 180.0, 206.565, 225.0, 243.435, 270.0, 296.565, 315.0, 333.435]
+% Heuristic file csv will contain 3200 rows and 16 cols, rows are
+% organized in col-major.
+% row 1 :  heuristic_cost of (1,1) cell for 16 theta's
+% row 2 :  heuristic_cost of (2,1) cell for 16 theta's
+% ...
+% row 40 : (40,1) cell
+% row 41 : (1,2) cell
+% ...
+% row 3200 : heuristic_cost of (40,80) cell for 16 theta's.
+
+filename = 'bl_heuristic.csv';
 csvwrite(filename,Hmap);
 
 %% Visualize
